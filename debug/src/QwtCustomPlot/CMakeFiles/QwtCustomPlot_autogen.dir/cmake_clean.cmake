e6d7f803677e5fabf63eaccc8289c6a0a5013bda
Paul von Bieberstein <paulbieberstein@googlemail.com>
2020-09-07 22:26:23 +0200

    removed build files

--- debug/src/QwtCustomPlot/CMakeFiles/QwtCustomPlot_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/QwtCustomPlot_autogen"
-  "QwtCustomPlot_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/QwtCustomPlot_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- debug/src/SplineFunctions/CMakeFiles/SplineFunctions_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SplineFunctions_autogen"
-  "SplineFunctions_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SplineFunctions_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
